70dd5037625f01c0c089bca6fe3b48d4dfca22d6
Victor42 <greenzorromail@gmail.com>
2015-02-12 02:03:57 +0800

    test

--- test/assets/js/functions.js
+++ test/assets/js/functions.js
@@ -2,6 +2,8 @@ $(function () {
 
 	// 页面效果
 	indexMenu();  //首页菜单切换
+	innerMenu();  //内页菜单
+	expandList();  //展开式列表
 
 	// 基础功能
 	document.body.addEventListener('touchstart', function () {});  //激活:active的触摸效果
@@ -16,6 +18,7 @@ $(function () {
 //                              页面效果                                //
 //*********************************************************************//
 
+
 // 首页菜单切换
 function indexMenu () {
 	if ($(".index").length > 0) {
@@ -29,6 +32,39 @@ function indexMenu () {
 				$(".menu ul").eq(index).addClass("active");
 			}
 		});
+	};
+}
+
+
+// 内页菜单
+function innerMenu () {
+	if ($(".toolbar").length > 0) {
+		var menu = $(".toolbar ul");
+		$(".toolbar .menu_btn").bind("touchstart", function () {
+			if (menu.hasClass("show")) {
+				menu.removeClass("show");
+			}
+			else {
+				menu.addClass("show");
+			}
+		});
+	};
+}
+
+
+// 展开式列表
+function expandList () {
+	if ($(".expand").length > 0) {
+		$(".expand li>a").each(function () {
+			$(this).bind("touchstart", function () {
+				if ($(this).parent().hasClass("show")) {
+					$(this).parent().removeClass("show");
+				}
+				else {
+					$(this).parent().addClass("show");
+				}
+			})
+		})
 	};
 }
 
