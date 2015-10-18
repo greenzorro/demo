aca18058a88f42533621411c1a9cd739663e7e0a
ColaChan <greenzorro@163.com>
2015-10-18 20:14:32 +0800

    test

--- share/assets/functions.js
+++ share/assets/functions.js
@@ -1,18 +1,19 @@
 var counter = 1;
 
-var loader = new resLoader({
-    resources : [
-        'test.hyperesources/bgmusic.mp3'
-    ],
-    onStart : function(total){
-    },
-    onProgress : function(current, total){
-    },
-    onComplete : function(total){
-        document.getElementById('loading').style.display = 'none';
-    }
-});
-loader.start();
+// var loader = new resLoader({
+//     resources : [
+//         'test.hyperesources/HYPE-466.full.min.js',
+//         'test.hyperesources/bgmusic.mp3'
+//     ],
+//     onStart : function(total){
+//     },
+//     onProgress : function(current, total){
+//     },
+//     onComplete : function(total){
+//         document.getElementById('loading').style.display = 'none';
+//     }
+// });
+// loader.start();
 
 window.onload = function () {
 
