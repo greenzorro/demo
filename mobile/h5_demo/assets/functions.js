524d3785f770b118395b330d6f794cf5a4665d0d
Victor42 <greenzorromail@gmail.com>
2015-10-31 22:21:31 +0800

    no message

--- mobile/h5_demo/assets/functions.js
+++ mobile/h5_demo/assets/functions.js
@@ -62,33 +62,6 @@
                 play_music();
                 $('.music').toggleClass('music_play');
             })
-            // var play_filter=$('.music')[0];
-            // play_filter.addEventListener('click', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('touchstart', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('touchend', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('touchmove', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('mousedown', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('mouseup', function(){
-            //     just_play(1);
-            // });
-            // play_filter.addEventListener('mousemove',function(){
-            //     just_play(1);
-            // });
-            // window.onload=function(){
-            //     if (!is_weixn()){
-            //         just_play();
-            //     }
-            // } 
             function play_music(){
                 if ($('.music').hasClass('music_play')){
                     $('.music audio').get(0).pause();
@@ -97,22 +70,6 @@
                 }
                 event.stopPropagation(); //阻止冒泡 
             }
-            // function just_play(id){
-            //     $('.music audio').get(0).play();
-            //     // if (typeof(id)!='undefined'){
-            //     //     $('#music_play_filter').hide();
-            //     // }
-            //     event.stopPropagation(); //阻止冒泡 
-            // } 
-            // function is_weixn(){
-            //     return false;
-            //     var ua = navigator.userAgent.toLowerCase();
-            //     if(ua.match(/MicroMessenger/i)=="micromessenger") {
-            //         return true;
-            //     } else {
-            //         return false;
-            //     }
-            // }
         },
         share: function () {  //显示隐藏右上角分享提示
             $('body').on('click','.share',function () {
